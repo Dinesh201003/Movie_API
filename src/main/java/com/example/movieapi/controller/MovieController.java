ec185616caf746cf8f7938f24034c8fc3f0763ad
Dinesh <msdinesh2003@gmail.com>
2026-02-12 07:58:55 +0530

    Enable CORS for frontend

--- src/main/java/com/example/movieapi/controller/MovieController.java
+++ src/main/java/com/example/movieapi/controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/movies")
+@CrossOrigin(origins = "*")
 public class MovieController {
 
     private final MovieService movieService;
